3e6c4ca4b6a0c2d895352bdc89dd56253ca9aa08
Mouzzam Tahir <moazzamtahir81@gmail.com>
2019-10-15 21:03:32 +0500

    states in swiftUI

--- day 16/Project 1 (Bill Split/Project 1 (Bill Split/ContentView.swift
+++ day 16/Project 1 (Bill Split/Project 1 (Bill Split/ContentView.swift
@@ -8,9 +8,43 @@
 
 import SwiftUI
 
+///some means one specific type of view has to be return
+///VIEW comes from swiftUI, which is the basic protocol to display anything on the screen
+///Forms converts the layout in to list order like the settings in iOS
+///we can use upto 10 lists in one form but if we need to use more list we need to group them
+///NavigationBar here called Navigation View.
+///state is the property wrapper that let us md
+
 struct ContentView: View {
+    @State var buttonTapped = 0
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Button(action: {
+                    self.buttonTapped += 1
+                }) {
+                    Text("button Tapped: \(self.buttonTapped)")
+                }
+                Section {
+                    Text("Hello SwiftUI")
+                    Text("Hello SwiftUI")
+                    Text("Hello SwiftUI")
+                    Text("Hello SwiftUI")
+                    Text("Hello SwiftUI")
+                }
+                
+                Group {
+                    Text("Hello SwiftUI")
+                    Text("Hello SwiftUI")
+                    Text("Hello SwiftUI")
+                    Text("Hello SwiftUI")
+                    Text("Hello SwiftUI")
+                }
+            }
+            .navigationBarTitle("SwiftUI")
+        }
+        
     }
 }
 
