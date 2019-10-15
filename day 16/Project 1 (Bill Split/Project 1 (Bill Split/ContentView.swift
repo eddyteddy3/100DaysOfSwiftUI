e2eff5f87ae2cd71226bc1ef5c4997dcb31c4ce6
Mouzzam Tahir <moazzamtahir81@gmail.com>
2019-10-15 21:10:37 +0500

    $ sign implementation

--- day 16/Project 1 (Bill Split/Project 1 (Bill Split/ContentView.swift
+++ day 16/Project 1 (Bill Split/Project 1 (Bill Split/ContentView.swift
@@ -13,19 +13,28 @@ import SwiftUI
 ///Forms converts the layout in to list order like the settings in iOS
 ///we can use upto 10 lists in one form but if we need to use more list we need to group them
 ///NavigationBar here called Navigation View.
-///state is the property wrapper that let us md
+///state is the property wrapper that let us modify the value
+///$ sign reads and writes the value for any State property
+///Changes will be reflected in real time
 
 struct ContentView: View {
     @State var buttonTapped = 0
-    
+    @State var name = ""
     var body: some View {
         NavigationView {
             Form {
+                
+                Section {
+                    TextField("Please enter your name", text: $name)
+                    Text("Your name is: \(name)")
+                }
+                
                 Button(action: {
                     self.buttonTapped += 1
                 }) {
                     Text("button Tapped: \(self.buttonTapped)")
                 }
+                
                 Section {
                     Text("Hello SwiftUI")
                     Text("Hello SwiftUI")
