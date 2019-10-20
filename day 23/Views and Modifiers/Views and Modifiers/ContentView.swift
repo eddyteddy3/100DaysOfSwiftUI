b359bbfc39441f25147e2ea9089f3c0510cc7cb9
Mouzzam Tahir <moazzamtahir81@gmail.com>
2019-10-20 23:44:17 +0500

    Modifiers Order

--- day 23/Views and Modifiers/Views and Modifiers/ContentView.swift
+++ day 23/Views and Modifiers/Views and Modifiers/ContentView.swift
@@ -5,12 +5,21 @@
 //  Created by Moazzam Tahir on 20/10/2019.
 //  Copyright © 2019 Moazzam Tahir. All rights reserved.
 //
+///Modifier Order matters because we call the modifier, it creates the new view and then stacks up the new view on top of it.
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         Text("Hello World")
+            .background(Color.yellow)
+            .padding()
+            .background(Color.blue)
+            .padding()
+            .background(Color.green)
+            .padding()
+            .background(Color.red)
     }
 }
 
