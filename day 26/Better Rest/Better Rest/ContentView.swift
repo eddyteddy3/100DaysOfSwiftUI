1fbcf0dd98e5955880169110f7a801e60c01b094
Mouzzam Tahir <moazzamtahir81@gmail.com>
2019-10-22 23:12:35 +0500

    Stepper View

--- day 26/Better Rest/Better Rest/ContentView.swift
+++ day 26/Better Rest/Better Rest/ContentView.swift
@@ -1,16 +1,22 @@
- //
+//
 //  ContentView.swift
 //  Better Rest
 //
 //  Created by Moazzam Tahir on 22/10/2019.
 //  Copyright © 2019 Moazzam Tahir. All rights reserved.
-//
+//MARK: Explanation
+///Stepper increment or decrement the value by specified number of value.
+///State is needed too as well
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var stepperValue = 8.0
+    
     var body: some View {
-        Text("Hello World")
+        Stepper(value: $stepperValue, in: 1...10, step: 0.25) {
+            Text("\(self.stepperValue, specifier: "%g") Hours")
+        }.padding()
     }
 }
 
