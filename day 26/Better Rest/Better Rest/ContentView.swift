4d7ad2c724f2326d4358ff8e24b64297cd050d9d
Mouzzam Tahir <moazzamtahir81@gmail.com>
2019-10-22 23:22:03 +0500

    Date picker added

--- day 26/Better Rest/Better Rest/ContentView.swift
+++ day 26/Better Rest/Better Rest/ContentView.swift
@@ -7,16 +7,25 @@
 //MARK: Explanation
 ///Stepper increment or decrement the value by specified number of value.
 ///State is needed too as well
+///Date Picker displays the picker of the date and the time
+///for the date picker, the state which is the instance of Date() is required as well to select the time
 
 import SwiftUI
 
 struct ContentView: View {
     @State private var stepperValue = 8.0
+    @State private var datePicker = Date()
     
     var body: some View {
-        Stepper(value: $stepperValue, in: 1...10, step: 0.25) {
-            Text("\(self.stepperValue, specifier: "%g") Hours")
-        }.padding()
+        VStack {
+            Stepper(value: $stepperValue, in: 1...10, step: 0.25) {
+                Text("\(self.stepperValue, specifier: "%g") Hours")
+            }.padding()
+            
+            DatePicker(selection: $datePicker, displayedComponents: .date) {
+                Text("Select Date").padding()
+            }
+        }
     }
 }
 
