b53f15eeeb7d870e7fa24e7a7207640bbc18a56a
Mouzzam Tahir <moazzamtahir81@gmail.com>
2019-10-23 17:46:46 +0500

    Functionality added

--- day 26/Better Rest/Better Rest/ContentView.swift
+++ day 26/Better Rest/Better Rest/ContentView.swift
@@ -14,8 +14,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var stepperValue = 8.0
-    @State private var sleepingHour = Date()
+    @State var sleepingHour = Date()
     @State private var coffeCups = 1
+    @State private var alertMessage = ""
+    @State private var showAlert = false
     
     var body: some View {
         NavigationView {
@@ -35,7 +37,37 @@ struct ContentView: View {
                 Stepper(value: $coffeCups, in: 1...20, step: 1) {
                     Text("\(self.coffeCups) Cups")
                 }.padding()
+                
+                    .alert(isPresented: $showAlert) { () -> Alert in
+                        Alert(title: Text("Your ideal time is"), message: Text(self.alertMessage), dismissButton: .default(Text("Gotcha!")))
+                }
             }
+            .navigationBarTitle("Better Rest")
+            .navigationBarItems(trailing:
+                Button(action: calculateSleep) {
+                    Text("Calculate")
+                }
+            )
+        }
+    }
+    
+    func calculateSleep() {
+        let sleepCalculator = BetterRest()
+        let timeComponents = Calendar.current.dateComponents([.hour, .minute], from: sleepingHour)
+        let minute = (timeComponents.minute ?? 0) * 60
+        let hour = (timeComponents.hour ?? 0) * 60 * 60
+        
+        do {
+            let predictions = try sleepCalculator.prediction(wake: Double(hour + minute), estimatedSleep: stepperValue, coffee: Double(coffeCups))
+            
+            let sleepTime = sleepingHour - predictions.actualSleep
+            
+            let timeString = DateFormatter()
+            timeString.timeStyle = .short
+            alertMessage = timeString.string(from: sleepTime)
+            showAlert = true
+        } catch let err {
+            print("Error: \(err.localizedDescription)")
         }
     }
 }
