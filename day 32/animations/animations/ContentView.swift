f170fc48f9e8485018c8c81454481fc363d69413
Mouzzam Tahir <moazzamtahir81@gmail.com>
2019-11-02 02:33:40 +0500

    day 32 added

---
+++ day 32/animations/animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  animations
+//
+//  Created by Moazzam Tahir on 02/11/2019.
+//  Copyright © 2019 Moazzam Tahir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
