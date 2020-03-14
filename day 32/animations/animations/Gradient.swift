b201dd71e17ca6b98346f31b141f8481c9a4f85a
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-03-14 21:50:16 +0500

    done with day 32

--- day 32/animations/animations/Gradient.swift
+++ day 32/animations/animations/Gradient.swift
@@ -8,9 +8,66 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -10, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct Gradient: View {
+    var letters = Array("Hello SwiftUI")
+    @State var enable: Bool = true
+    @State var dragAmount = CGSize.zero
+    @State var isShowing: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            VStack {
+                Button("Tap me"){
+                    withAnimation {
+                        self.isShowing.toggle()
+                    }
+                }
+                
+                if isShowing {
+                    Rectangle()
+                        .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+                }
+                
+            }
+            
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count) { num in
+                    Text(String(self.letters[num]))
+                    .padding(5)
+                    .background(self.enable ? Color.red : Color.blue)
+                    .offset(self.dragAmount)
+                    .animation(Animation.linear.delay(Double(num) / 20))
+                }
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged({
+                        self.dragAmount = $0.translation
+                    })
+                    .onEnded({_ in
+                        self.dragAmount = CGSize.zero
+                        self.enable.toggle()
+                    })
+            )
+        }
     }
 }
 
