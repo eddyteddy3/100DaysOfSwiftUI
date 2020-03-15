922d9713233a6a8da469b56246bd141fc496f2b5
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-03-16 01:27:08 +0500

    started working with Published and Observeable objects

--- day 36/iExpense/iExpense/ContentView.swift
+++ day 36/iExpense/iExpense/ContentView.swift
@@ -8,9 +8,21 @@
 
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var firstName = "Hello" //publish will make an annoucment that these variable changes to any view
+    @Published var lastName = "World"
+}
+
 struct ContentView: View {
+    @ObservedObject var user = User() //ObservedObjects will listen to those annoucemets
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("First name: \(user.firstName), Last name: \(user.lastName)")
+            
+            TextField("name", text: $user.firstName)
+            TextField("last", text: $user.lastName)
+        }
     }
 }
 
