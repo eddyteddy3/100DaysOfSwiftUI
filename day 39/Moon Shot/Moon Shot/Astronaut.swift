8b7234ad5ff13e868faa084a9618309a860ea8cd
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-05-03 02:24:04 +0500

    Started day 40

---
+++ day 39/Moon Shot/Moon Shot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moon Shot
+//
+//  Created by Moazzam Tahir on 03/05/2020.
+//  Copyright © 2020 Moazzam Tahir. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    var id: String
+    var name: String
+    var description: String
+}
---
+++ day 39/Moon Shot/Moon Shot/BundleExtension.swift
@@ -0,0 +1,15 @@
+//
+//  BundleExtension.swift
+//  Moon Shot
+//
+//  Created by Moazzam Tahir on 03/05/2020.
+//  Copyright © 2020 Moazzam Tahir. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(fileName name: String) -> [Astronaut] {
+        
+    }
+}
