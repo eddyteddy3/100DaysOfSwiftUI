13e0ebf62e441a606e37d7ad4b13fbea078b8225
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-05-12 06:26:31 +0500

    added arc and triangle shape

--- day 41/Drawing/Drawing/ContentView.swift
+++ day 41/Drawing/Drawing/ContentView.swift
@@ -8,9 +8,43 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startingAngle: Angle
+    let endingAngle: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let adjustmentAngel = Angle.degrees(90)
+        let adjustedStartAngle = startingAngle - adjustmentAngel
+        let adjustedEndAngle = endingAngle - adjustmentAngel
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: adjustedStartAngle, endAngle: adjustedEndAngle, clockwise: !clockwise)
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Arc(startingAngle: Angle(degrees: 0), endingAngle: Angle(degrees: 90), clockwise: true)
+            .stroke(style: StrokeStyle(lineWidth: 12, lineCap: .round))
+            .frame(width: 300, height: 300)
+        
     }
 }
 
