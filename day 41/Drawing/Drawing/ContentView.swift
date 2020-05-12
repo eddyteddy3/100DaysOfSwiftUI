02e9261f64dd4015706b5de2cf56a82220c4448a
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-05-12 16:21:23 +0500

    added flower shape struct

--- day 41/Drawing/Drawing/ContentView.swift
+++ day 41/Drawing/Drawing/ContentView.swift
@@ -22,7 +22,6 @@ struct Triangle: Shape {
 }
 
 struct Arc: Shape, InsettableShape {
-    
     let startingAngle: Angle
     let endingAngle: Angle
     let clockwise: Bool
@@ -45,6 +44,38 @@ struct Arc: Shape, InsettableShape {
         var arc = self
         arc.insetAmount += amount
         return arc
+    }
+}
+
+struct Flower: Shape {
+    //how much flower move away from the center
+    var petalOffset: Double = -20
+    
+    //how wide is each petal
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        //path to hold all the petals
+        var path = Path()
+        
+        //count from 0 up to .pi*8, moving up to .pi / 8
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            //rotating the petal using the current value of loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // moving the petal to the center of the view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            //creating fixed petal height and y position
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalOffset), height: rect.width / 2))
+            
+            //applying the petal rotation
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
     }
 }
 
